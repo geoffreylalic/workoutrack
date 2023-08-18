54265b5136cc0a732824522413060bf9a4d934ff
Geoffrey <geoffrey0151@gmail.com>
2023-08-19 00:11:41 +0200

    Initial commit

---
+++ src/main/java/com/springboot/workoutrack/WorkoutrackApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.workoutrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/workoutrack/WorkoutrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.workoutrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
